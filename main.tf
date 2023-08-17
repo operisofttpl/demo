12413ea8858290e1586a64c55b78f6f9e40aad83
operisofttpl <141032066+operisofttpl@users.noreply.github.com>
2023-08-17 14:49:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"  # Replace with your desired AWS region
+  region = "ap-southeast-1"  # Replace with your desired AWS region
 }
 
 resource "tls_private_key" "master-key-gen" {
