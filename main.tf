0758ea054374d7c2f4c05866156a3ad79c94d3fa
operisofttpl <141032066+operisofttpl@users.noreply.github.com>
2023-08-17 14:36:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-south-1"  # Replace with your desired AWS region
+}
+
+resource "tls_private_key" "master-key-gen" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the Key Pair of kali linux didnt have software
+resource "aws_key_pair" "master-key-pair" {
+  key_name   = "keypair"
+  public_key = tls_private_key.master-key-gen.public_key_openssh
+}
+
+resource "aws_instance" "demo_ec2" {
+  ami           = "ami-0df7a207adb9748c7"  # Replace with your desired AMI ID
+  instance_type = "t3a.small"  # Replace with your desired instance type
+  key_name      = aws_key_pair.master-key-pair.key_name  
+  tags = {
+    Name = "demo"
+  }
+
+output "pem_file_for_ssh" {
+  value = tls_private_key.master-key-gen.private_key_pem
+  sensitive = true
+}
+
+output "ec2" {
+  value = aws_instance.demo_ec2.public_ip
+}
