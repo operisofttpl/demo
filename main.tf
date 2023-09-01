b017ae2161d8ad4d0fd705dab2449763bbbfde37
operisofttpl <141032066+operisofttpl@users.noreply.github.com>
2023-09-01 20:27:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-southeast-1"  # Replace with your desired AWS region
+  region = "ap-southeast-1"
 }
 
 resource "tls_private_key" "master-key-gen" {
@@ -7,15 +7,14 @@ resource "tls_private_key" "master-key-gen" {
   rsa_bits  = 4096
 }
 
-# Create the Key Pair of kali linux didnt have software
 resource "aws_key_pair" "master-key-pair" {
   key_name   = "keypair"
   public_key = tls_private_key.master-key-gen.public_key_openssh
 }
 
 resource "aws_instance" "demo_ec2" {
-  ami           = "ami-0df7a207adb9748c7"  # Replace with your desired AMI ID
-  instance_type = "t3a.small"  # Replace with your desired instance type
+  ami           = "ami-0df7a207adb9748c7"
+  instance_type = "t3a.small"
   key_name      = aws_key_pair.master-key-pair.key_name  
   tags = {
     Name = "demo"
